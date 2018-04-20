d1faf5ad2ef9b79658f9b057d6f0266325a6dc8b
Fenix <fenix117@qq.com>
2018-04-20 17:19:02 +0800

    更改登录逻辑1

---
+++ DataEntity/EntityModel/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEntity.EntityModel
+{
+    public class Users
+    {
+        public System.Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string TenantsId { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> Status { get; set; }
+    }
+}
